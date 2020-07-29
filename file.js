548da21298af678ea5661dfdfea89d94feccc0f9
abhittac <59259249+abhittac@users.noreply.github.com>
2020-07-29 17:10:00 +0530

    Add files via upload

---
+++ file.js
@@ -0,0 +1,49 @@
+var s1 = document.getElementById("mysound1"); 
+var s2 = document.getElementById("mysound2"); 
+var s3 = document.getElementById("mysound3"); 
+var s4 = document.getElementById("mysound4"); 
+var s5 = document.getElementById("mysound5"); 
+var s6 = document.getElementById("mysound6"); 
+
+var t1 = document.getElementById("tune1"); 
+var t2 = document.getElementById("tune2"); 
+var t3 = document.getElementById("tune3"); 
+
+function play1Audio() { 
+  s1.play(); 
+} 
+
+function play2Audio() { 
+  s2.play(); 
+}
+function play3Audio() { 
+  s3.play(); 
+}
+function play4Audio() { 
+  s4.play(); 
+}
+function play5Audio() { 
+  s5.play(); 
+}
+function play6Audio() { 
+  s6.play(); 
+}
+
+function tun1Audio() {
+
+  t1.play()
+  }
+
+function tun1AudioStop() { 
+  t1.pause()
+ }
+  function tun2Audio() {
+
+    t2.play()
+    }
+    function tun2AudioStop() {
+
+      t2.pause()
+      }
+ 
+ 
